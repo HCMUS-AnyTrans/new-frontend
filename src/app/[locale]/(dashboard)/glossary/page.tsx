f56bd4bb4cbf76d42c291947864ba18a9ba76ec2
MinhNguyenCG <info.nguyen1104@gmail.com>
2026-03-03 14:18:19 +0700

    fix width for history page and gloosary page

--- src/app/[locale]/(dashboard)/glossary/page.tsx
+++ src/app/[locale]/(dashboard)/glossary/page.tsx
@@ -4,7 +4,7 @@ import { GlossaryContent } from "@/features/glossary";
 
 export default function GlossaryPage() {
   return (
-    <div className="flex flex-col gap-6 p-4 lg:p-6">
+    <div className="flex flex-col gap-6 p-4 lg:p-6 mx-auto w-full max-w-5xl">
       <GlossaryContent />
     </div>
   );
--- src/app/[locale]/(dashboard)/history/page.tsx
+++ src/app/[locale]/(dashboard)/history/page.tsx
@@ -4,7 +4,7 @@ import { HistoryContent } from "@/features/history";
 
 export default function HistoryPage() {
   return (
-    <div className="flex flex-col gap-6 p-4 lg:p-6">
+    <div className="flex flex-col gap-6 p-4 lg:p-6 mx-auto w-full max-w-5xl">
       <HistoryContent />
     </div>
   );
